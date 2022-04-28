8e810d444e15de8865ffd801f2f07d3b67b6f1b1
motchm <92804075+motchm@users.noreply.github.com>
2022-04-28 13:29:34 +0200

    Update dbbackup.sh

--- dbbackup.sh
+++ dbbackup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#CHMOD 700 THIS FILE!
+
 echo Backup initialized && \
 mysqldump -u root --password=<PASSWORD> --all-databases > /home/<USERNAME>/testdb.sql && \
 echo Backup finished
