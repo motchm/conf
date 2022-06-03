3986b7317985d463c121d31d7882e6bcf8cd6bd3
motchm <92804075+motchm@users.noreply.github.com>
2022-06-03 22:21:25 +0200

    Update ubuntu22setup.sh

--- ubuntu22setup.sh
+++ ubuntu22setup.sh
@@ -10,16 +10,23 @@ apt install snapd psmisc neofetch htop ufw nano bash-completion docker.io -y
 apt install ubuntu-advantage-tools
 ua attach <TOKEN>
 
-#configure
+#passwordless sudo
 echo 'motch ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+
+#set up docker
 systemctl enable --now docker
 usermod -aG docker <USER>
+
+#set up ssh
 ssh-keygen
 ufw allow ssh
+
+#free port 53 for pihole
 systemctl disable systemd-resolved
 echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
 cat /etc/resolv.conf > /etc/resolve.temp
 rm /etc/resolv.conf
 mv /etc/resolve.temp /etc/resolv.conf
+
 #upload ssh-key of management machine
 #set password authentification to no in /etc/ssh/sshd_config
